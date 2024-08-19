4c578219b8cb10096cf5d46be5e2f0e51b31341a
Eloy Coto <eloy.coto@acalustra.com>
2024-08-19 12:28:12 +0200

    Add IBM watsonX packages
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- flake.nix
+++ flake.nix
@@ -10,6 +10,124 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
+
+          ibmCosSDKCore = pkgs.python3Packages.buildPythonPackage rec {
+            name = "ibm-cos-sdk-core";
+            pname = "ibm-cos-sdk-core";
+            version="2.13.6";
+            pyproject=true;
+
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              hash = "sha256-3UH7eJ7rZVRlAa+rzVDniEarRRO2rUBC5BC2oU/4hBM=";
+            };
+            build-system = [ pkgs.python3Packages.poetry-core ];
+            dependencies = [
+              pkgs.python3Packages.setuptools
+            ];
+            propagatedBuildInputs =  [ ];
+            pythonImportsCheck = [  ];
+            pythonRuntimeDepsCheckHook = ''
+              echo "test"
+            '';
+
+            pythonRuntimeDepsCheck = ''
+              ls -lh
+              echo "hello"
+            '';
+            doCheck = false;
+          };
+
+          ibmCosSDK = pkgs.python3Packages.buildPythonPackage rec {
+            name = "ibm-cos-sdk";
+            pname = "ibm-cos-sdk";
+            version="2.0.1";
+            pyproject=true;
+
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              hash = "sha256-NG0neo50uuggtNagP2EHlst6d/+FQaZztqob0YV6hj4=";
+            };
+            build-system = [ pkgs.python3Packages.poetry-core ];
+            dependencies = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.jmespath
+              ibmCosSDKCore
+            ];
+            propagatedBuildInputs =  [ ];
+            pythonImportsCheck = [  ];
+            pythonRuntimeDepsCheckHook = ''
+              echo "test"
+            '';
+
+            pythonRuntimeDepsCheck = ''
+              ls -lh
+              echo "hello"
+            '';
+            doCheck = false;
+          };
+
+          ibmWatsonAI = pkgs.python3Packages.buildPythonPackage rec {
+            name = "ibm-watsonx-ai";
+            pname = "ibm_watsonx_ai";
+            version="1.1.5";
+            pyproject=true;
+
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              hash = "sha256-UGphdTqomT+J3QFNBRr77BwufplhutAfs8yCpRoMGiE=";
+            };
+            build-system = [ pkgs.python3Packages.poetry-core ];
+            dependencies = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.pandas
+              pkgs.python3Packages.httpx
+              pkgs.python3Packages.tabulate
+              pkgs.python3Packages.langchain
+              pkgs.python3Packages.importlib-metadata
+              pkgs.python3Packages.lomond
+              ibmCosSDK
+            ];
+            propagatedBuildInputs =  [ ];
+            pythonImportsCheck = [  ];
+            pythonRuntimeDepsCheckHook = ''
+              echo "test"
+            '';
+
+            pythonRuntimeDepsCheck = ''
+              ls -lh
+              echo "hello"
+            '';
+            doCheck = false;
+          };
+          langchain-ibm = pkgs.python3Packages.buildPythonPackage rec {
+            name = "langchain-ibm";
+            pname = "langchain_ibm";
+            version="0.1.12";
+            pyproject=true;
+
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              hash = "sha256-okKNW3dUElv4/YVFslz6r8df9FsHzKFyQT2hxu6QfK4=";
+            };
+
+            build-system = [ pkgs.python3Packages.poetry-core ];
+            dependencies = [
+              ibmWatsonAI
+            ];
+            propagatedBuildInputs =  [ ];
+            pythonImportsCheck = [  ];
+            pythonRuntimeDepsCheckHook = ''
+              echo "test"
+            '';
+
+            pythonRuntimeDepsCheck = ''
+              ls -lh
+              echo "hello"
+            '';
+            doCheck = false;
+          };
+
           pkgs = import nixpkgs {
             inherit system;
           };
@@ -58,6 +176,8 @@
               '';
               doCheck = false;
             }))
+            ibmWatsonAI
+            langchain-ibm
           ];
 
         in
