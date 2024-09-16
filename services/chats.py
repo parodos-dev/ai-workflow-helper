c12bb340944994089262c90b562b7437c369c16d
Eloy Coto <eloy.coto@acalustra.com>
2024-09-16 12:54:30 +0200

    refactor: moving things to be a bit cleaner

--- services/chats.py
+++ services/chats.py
@@ -1,3 +1,5 @@
+import logging
+
 from const import SYSTEM_MESSAGE, EXAMPLES
 from lib.models import SerVerlessWorkflow
 from lib.validator import JsonSchemaValidationException
@@ -9,7 +11,8 @@ from langchain.prompts import FewShotChatMessagePromptTemplate
 from langchain.prompts import MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
-WORKFLOWS={}
+WORKFLOWS = {}
+
 
 def get_prompt_details():
     example_prompt = ChatPromptTemplate.from_messages(
@@ -40,8 +43,10 @@ def get_prompt_details():
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
+
 def get_workflow_for_session(ctx, session_id):
-    return WORKFLOWS.get(session_id, {})
+    return WORKFLOWS.get(str(session_id), {})
+
 
 def get_history(ctx, session_id):
     ctx.history_repo.set_session(session_id)
@@ -49,25 +54,25 @@ def get_history(ctx, session_id):
 
 
 def set_json_response(session_id, ai_response, validator):
-    try:
-        workflow =validator.get_json(ai_response)
-        document = validator.invoke(workflow)
-        if document is not None:
-            print("No valid document")
-        WORKFLOWS[session_id] = {
-                "document": document,
-                "valid": True
-        }
-    except (ParsedOutputException) as e:
-        print(f"Invalid JSON document: {e}")
-    except (JsonSchemaValidationException) as e:
+    def set_workflow(doc, valid):
         WORKFLOWS[str(session_id)] = {
-                "document": workflow,
-                "valid": False
+            "document": doc,
+            "valid": valid
         }
-        print("Invalid JSON document for session", session_id)
+
+    try:
+        document = validator.invoke(ai_response)
+        if document is None:
+            logging.info("document is null")
+            return
+        set_workflow(document, True)
+    except (ParsedOutputException) as e:
+        logging.error(f"cannot get valid JSON document from the response: {e}")
+    except (JsonSchemaValidationException) as e:
+        set_workflow(e.data, False)
+        logging.error("workflow is not correct has some JSON issues")
     except Exception as e:
-        print("Invalid Exception document")
+        logging.error(f"Unexected exception on the workflow: {e}")
 
 
 def get_response_for_session(ctx, session_id, user_message):
@@ -103,7 +108,7 @@ def get_response_for_session(ctx, session_id, user_message):
         ai_response.append(x.content)
         yield x.content
 
-    full_ai_response =  "".join(ai_response)
+    full_ai_response = "".join(ai_response)
     set_json_response(session_id, full_ai_response, ctx.validator)
     # history_repo.add_user_message(user_message)
     # history_repo.add_ai_message(full_ai_response)
