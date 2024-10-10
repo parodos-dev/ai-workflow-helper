79a6021e1a4f43a8fadcc9f7fbf356fb8cd7c460
Eloy Coto <eloy.coto@acalustra.com>
2024-10-10 12:52:11 +0200

    feat: better errors in jsonschema
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- lib/json_validator.py
+++ lib/json_validator.py
@@ -2,16 +2,37 @@ from jsonschema import ValidationError, Draft202012Validator
 import requests
 import yaml
 import json
-import jsonschema
+
 
 def format_error(error: ValidationError) -> dict:
-    best_match = jsonschema.exceptions.best_match(error.context)
+
+    underlaying = []
+    state_type = error.instance.get("type")
+    definition = f"#/definitions/{state_type}state"
+    validator_schema = error.schema
+
+    if state_type and error.validator in ["anyOf"]:
+        path = None
+        for i,x in enumerate(validator_schema.get(error.validator)):
+            if x.get("$ref") == definition:
+                path = i
+                break
+        i = 0
+        for x in error.context:
+            if x.schema_path[0] != path:
+                continue
+            subschema_path= ".".join([str(val) for val in x.schema_path])
+            underlaying_error = f"\tSuberrror {i} in definition: {definition}\n"
+            underlaying_error += f"\t\tFailed validating {x.validator!r} in {subschema_path}\n"
+            underlaying_error += f"\t\tValidator value: ```{json.dumps(x.validator_value)}```"
+            underlaying.append(underlaying_error.rstrip())
+            i += 1
     return {
-        "path": " -> ".join(str(path) for path in error.path),
+        "path": ".".join(str(path) for path in error.path),
         "message": error.message,
-        "schema_path": " -> ".join(str(path) for path in error.schema_path),
-        "instance": error.instance,
-        "underlaying": best_match.message
+        "schema_path": ".".join(str(path) for path in error.schema_path),
+        "instance": json.dumps(error.instance),
+        "underlaying": "\n".join(underlaying)
     }
 
 class JsonSchemaValidationException(Exception):
@@ -30,13 +51,13 @@ class JsonSchemaValidationException(Exception):
 
     def get_error(self):
         error_msg = [format_error(x) for x in self.errors]
+
         output = ""
         for i,x in enumerate(error_msg):
-            output += f"Error {i} in path {x['path']}\n"
-            output += f"\tMessage: {x['message']}\n"
+            output += f"Error {i} in path `{x['path']}`:\n"
             output += f"\tSchema_path: {x['schema_path']}\n"
-            output += f"\tInstance: {x['instance']}\n"
-            output += f"\tUnderlaying error: {x['underlaying']}\n"
+            output += f"\tInstance: ```{x['instance']}```\n"
+            output += f"\tUnderlaying errors:\n{x['underlaying']}\n"
             output += "\n"
         return output
 
@@ -80,7 +101,7 @@ class JsonSchemaValidatorTool():
             local_data = json.loads(data)
 
         validator = Draft202012Validator(self.json_schema_validator)
-        errors = sorted(validator.iter_errors(local_data), key=lambda e: e.path)
+        errors = list(validator.iter_errors(local_data))
         if len(errors) > 0:
             raise JsonSchemaValidationException(errors, local_data)
 
